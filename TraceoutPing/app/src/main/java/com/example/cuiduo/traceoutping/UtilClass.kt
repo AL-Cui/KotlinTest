3541f172117242694e21df6c91698f8ecdc86ae0
cuiduo <CUI_DUO@example.com>
2017-08-22 09:00:53 +0800

    traceping
    Android模拟traceroute ping

---
+++ TraceoutPing/app/src/main/java/com/example/cuiduo/traceoutping/UtilClass.kt
@@ -0,0 +1,23 @@
+package com.example.cuiduo.traceoutping
+
+/**
+ * Created by cuiduo on 2017/8/17.
+ */
+public object UtilClass {
+    @JvmStatic
+    val PING = "PING"
+    @JvmStatic
+    val FROM_PING = "From"
+    @JvmStatic
+    val SMALL_FROM_PING = "from"
+    @JvmStatic
+    val PARENTHESE_OPEN_PING = "("
+    @JvmStatic
+    val PARENTHESE_CLOSE_PING = ")"
+    @JvmStatic
+    val TIME_PING = "time="
+    @JvmStatic
+    val EXCEED_PING = "exceed"
+    @JvmStatic
+    val UNREACHABLE_PING = "100%"
+}
---
+++ TraceoutPing/app/src/main/java/com/example/cuiduo/traceoutping/fhduh.java
@@ -0,0 +1,9 @@
+package com.example.cuiduo.traceoutping;
+
+/**
+ * Created by cuiduo on 2017/8/17.
+ */
+
+public class fhduh {
+    String ss = UtilClass.getFROM_PING();
+}
